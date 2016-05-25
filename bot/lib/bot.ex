6106a7f4973c4125956ac2f272e5c131c327afc4
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-05-24 23:53:46 -0500

    Adding server to supervision

--- bot/lib/bot.ex
+++ bot/lib/bot.ex
@@ -7,8 +7,7 @@ defmodule Bot do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Bot.Worker, [arg1, arg2, arg3]),
+      worker(Bot.Server, [System.get_env("PORT")])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
